50fb2b3437ad3c835c2ec21c5c2a4094b9c02d27
Kenneth Rangel <kenneth.rangel-castro@quarters.com>
2020-08-06 11:35:47 +0200

    RNAdyenThreeDS2

---
+++ ios/RNAdyenThreeDS2/RNAdyenThreeDS2.h
@@ -0,0 +1,16 @@
+//
+//  RNAdyenThreeDS2.h
+//  RNAdyen
+//
+//  Created by Kenneth Castro on 06.08.20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNAdyenThreeDS2 : NSObject <RCTBridgeModule>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/RNAdyenThreeDS2/RNAdyenThreeDS2.m
@@ -0,0 +1,23 @@
+//
+//  RNAdyenThreeDS2.m
+//  RNAdyen
+//
+//  Created by Kenneth Castro on 06.08.20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import "RNAdyenThreeDS2.h"
+#import <react_native_adyen_encrypt-Swift.h>
+
+@implementation RNAdyenThreeDS2
+
+RCT_EXPORT_MODULE(RNAdyenThreeDS2);
+
+RCT_EXPORT_METHOD(encryptWithData: (NSDictionary *)data) {
+  
+    
+}
+
+
+@end
+
