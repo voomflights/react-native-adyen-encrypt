220dac9d7611ca19853ef7d8a186e39f3c7389e3
Kenneth Rangel <kenneth.rangel-castro@quarters.com>
2020-08-07 15:06:35 +0200

    add files

--- ios/RNAdyenEncryptor/AdyenThreeDS2Wrapper.swift
+++ ios/RNAdyenEncryptor/AdyenThreeDS2Wrapper.swift
@@ -10,13 +10,16 @@ import Adyen3DS2
 @objc public class AdyenThreeDS2Wrapper: NSObject {
     
     @objc public func identify(fingerprintToken: String?, resolve: RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) {
-       
-     
+
+       guard let fingerprintToken = fingerprintToken else { return }
+      let helper = HelperThreeDS2()
       resolve("caca")
      }
+    
     @objc public func challenger(challengeToken: String?, resolve: RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) {
-       
-     
+
+       guard let challengeToken = challengeToken else { return }
+     let helper = HelperThreeDS2()
         resolve("caca")
      }
     
---
+++ ios/RNAdyenEncryptor/HelperThreeDS2.swift
@@ -0,0 +1,13 @@
+//
+//  HelperThreeDS2.swift
+//  RNAdyen
+//
+//  Created by Kenneth Castro on 07.08.20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
+
+public struct HelperThreeDS2 {
+    
+}
